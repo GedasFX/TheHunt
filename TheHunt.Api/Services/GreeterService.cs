c9e339544f3793866bc4c44b6a36950712251a62
Gediminas Milašius <gediminasmilasius@gmail.com>
2022-10-15 19:33:07 +0300

    a

--- TheHunt.Api/Services/GreeterService.cs
+++ TheHunt.Api/Services/GreeterService.cs
@@ -1,27 +1,31 @@
 using Grpc.Core;
-using TheHunt.Api;
+using MediatR;
 
 namespace TheHunt.Api.Services;
 
-[MediatorService]
 public class GreeterService : Greeter.GreeterBase
 {
     private readonly ILogger<GreeterService> _logger;
+    private readonly IMediator _mediator;
 
-    public GreeterService(ILogger<GreeterService> logger)
+    public GreeterService(ILogger<GreeterService> logger, IMediator mediator)
     {
         _logger = logger;
+        _mediator = mediator;
     }
 
     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new HelloReply
-        {
-            Message = "Hello " + request.Name
-        });
+        return _mediator.Send(request);
     }
-}
 
-public class MediatorServiceAttribute : Attribute
-{
+    public override Task<HelloReply> SayHello2(HelloRequest request, ServerCallContext context)
+    {
+        return _mediator.Send(request);
+    }
+
+    public override Task<GetStateResponse> GetState(GetStateQuery request, ServerCallContext context)
+    {
+        return _mediator.Send(request);
+    }
 }
